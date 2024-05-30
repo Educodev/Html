4e32f130c8bee01baef2c2b222c2d485a4363c2e
Eduardo Herrera <eduardoherrera@educodev.com>
2024-05-30 19:28:52 +0200

    Initial Commit

---
+++ Html/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Html
+//
+//  Created by Eduardo Herrera on 30/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Html/HtmlApp.swift
@@ -0,0 +1,17 @@
+//
+//  HtmlApp.swift
+//  Html
+//
+//  Created by Eduardo Herrera on 30/5/24.
+//
+
+import SwiftUI
+
+@main
+struct HtmlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
